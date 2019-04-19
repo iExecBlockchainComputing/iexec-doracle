dd9166de5baafa0acf445f2d9e757f93f3900246
Hadrien Croubois <hc@iex.ec>
2019-04-19 14:30:00 +0200

    minimal documentation

---
+++ README.md
@@ -0,0 +1,51 @@
+iExec Dencentralized Oracle System
+==================================
+
+About iExec
+-----------
+
+Thanks to iExec, it is possible to achieve onchain consensus about the result of an offchain application. Applications are represented by smart contracts and executions results can be made available onchain with all the necessary proof.
+
+Building an oracle application
+------------------------------
+
+iExec applications produce different outputs.
+- The consensus is achieve on a deterministic value describing the application output. By default this is the hash of the result archive, but can be overriden by the content of `/iexec_out/determinism.iexec`. Upon succesfull verification, this is stored onchain in the `task.resultDigest` field.
+- The actual result. By default this is the IPFS address of a (potentially encrypted) archive containing the outputs, but can be overrident by the content of `/iexec_out/callback.iexec`. Upon succesfull verification, this is stored onchain in the `task.results` field.
+
+An iExec oracle application sur as the on used in the price-oracle example uses this 2 elements to produce verified results to the blockchain.
+
+Given a set of parameters, the application produce a self describing result, encodes it in a way that can be interpreted onchain, store it in `/iexec_out/callback.iexec` so that is can be accessed onchain, and store the hash of this encoded value to perform the consensus.
+
+For example, given the parameters "BTC USD 9 2019-04-11T13:08:32.605Z" the price-oracle application will:
+
+	1. Retreive the price of BTC in USD at 2019-04-11T13:08:32.605Z
+	2. Multiply this value by 10e9 (to capture price value more accurately as it will be represented by an integer onchain)
+	3. encode the date, the description ("btc-usd-9") and the value using `abi.encode`
+	4. Store this result in `/iexec_out/callback.iexec`
+	5. hash the result and store it in `/iexec_out/determinism.iexec`
+
+iExec will then achieve PoCo consensus on the `/iexec_out/determinism.iexec` value, and will store both the `/iexec_out/determinism.iexec` and the `/iexec_out/callback.iexec` onchain.
+
+Given a taskID, it is possible to retreive all the details of the computation as described above. The oracle smartcontract just needs to retreive the informations, verify the validity of the execution and process the encoded result. Thanks to the PoCo consensus, anyone can require a computation and ask the oracle to update itself in a trustless manner.
+
+How to setup an oracle contract
+-------------------------------
+
+	1. Record the address of the iExec Hub and Clerk contracts
+	2. Register the requierements needed for an result to we processed
+		a. Which application (single, any, whitelist?)
+		b. Which dataset (single, any, whitelist?)
+		c. Which workerpool (single, any, whitelist?)
+		d. Minimum level of trust
+		e. Mandatory tag
+
+How to update an oracle contract
+--------------------------------
+
+	1. Send the taskID of a valid execution to the oracle smart contract.
+	2. The oracle smart contract retreives details about this task from the iexec's smart contracts
+	3. The oracle smart contract verifies the execution is valid (authorized app, dataset, workerpool, trust level and tags)
+	4. The oracle smart contract verifies the hash of the results correspond to the resultDigest that achieved consensus, thus verifying the validity of the result field.
+	5. The oracle smart contract decodes the results using `abi.decode`
+	6. The oracle smart contract process this results. In the case of the price oracle this means storing the value if it is more recent than the one currently reccorded.
--- contracts/IexecOracleReceiver.sol
+++ contracts/IexecOracleReceiver.sol
@@ -1,16 +1,19 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "iexec-poco/contracts/IexecClerk.sol";
 import "iexec-poco/contracts/IexecHub.sol";
 
-contract IexecOracleReceiver is SignatureVerifier, IOracleConsumer
+contract IexecOracleReceiver is Ownable, SignatureVerifier, IOracleConsumer
 {
 	IexecHub   public m_iexecHub;
 	IexecClerk public m_iexecClerk;
 	address    public m_authorizedApp;
 	address    public m_authorizedDataset;
 	address    public m_authorizedWorkerpool;
+	bytes32    public m_requiredtag;
+	uint256    public m_requiredtrust;
 
 	struct timedValue
 	{
@@ -23,19 +26,27 @@ contract IexecOracleReceiver is SignatureVerifier, IOracleConsumer
 	event ResultReady(bytes32 indexed oracleCallId);
 	event ValueChange(bytes32 indexed id, uint256 oldDate, uint256 oldValue, uint256 newDate, uint256 newValue);
 
-	constructor(
-		address _iexecHub
-	,	address _authorizedApp
-	,	address _authorizedDataset
-	,	address _authorizedWorkerpool
-	)
+	constructor(IexecHub _iexecHub)
 	public
 	{
-		m_iexecHub             = IexecHub(_iexecHub);
-		m_iexecClerk           = IexecClerk(m_iexecHub.iexecclerk());
+		m_iexecHub             = _iexecHub;
+		m_iexecClerk           = m_iexecHub.iexecclerk();
+	}
+
+	function setEnv(
+		address _authorizedApp
+	,	address _authorizedDataset
+	,	address _authorizedWorkerpool
+	, bytes32 _requiredtag
+	, uint256 _requiredtrust
+	)
+	public onlyOwner
+	{
 		m_authorizedApp        = _authorizedApp;
 		m_authorizedDataset    = _authorizedDataset;
 		m_authorizedWorkerpool = _authorizedWorkerpool;
+		m_requiredtag          = _requiredtag;
+		m_requiredtrust        = _requiredtrust;
 	}
 
 	function receiveResult(bytes32 _oracleCallId, bytes calldata)
@@ -50,11 +61,13 @@ contract IexecOracleReceiver is SignatureVerifier, IOracleConsumer
 	)
 	internal view returns (bool)
 	{
-		require(task.status      == IexecODBLibCore.TaskStatusEnum.COMPLETED,                                       "result-not-available"             );
-		require(task.resultDigest == keccak256(task.results),                                                       "result-not-validated-by-consensus");
-		require(checkIdentity(m_authorizedApp,        deal.app.pointer,        m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-app"                 );
-		require(checkIdentity(m_authorizedDataset,    deal.dataset.pointer,    m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-dataset"             );
-		require(checkIdentity(m_authorizedWorkerpool, deal.workerpool.pointer, m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-workerpool"          );
+		require(task.status == IexecODBLibCore.TaskStatusEnum.COMPLETED,                                                                                    "result-not-available"             );
+		require(task.resultDigest == keccak256(task.results),                                                                                               "result-not-validated-by-consensus");
+		require(m_authorizedApp        == address(0) || checkIdentity(m_authorizedApp,        deal.app.pointer,        m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-app"                 );
+		require(m_authorizedDataset    == address(0) || checkIdentity(m_authorizedDataset,    deal.dataset.pointer,    m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-dataset"             );
+		require(m_authorizedWorkerpool == address(0) || checkIdentity(m_authorizedWorkerpool, deal.workerpool.pointer, m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-workerpool"          );
+		require(m_requiredtag & ~deal.tag == bytes32(0),                                                                                                    "invalid-tag"                      );
+		require(m_requiredtrust <= deal.trust,                                                                                                              "invalid-trust"                    );
 		return true;
 	}
 
@@ -64,9 +77,6 @@ contract IexecOracleReceiver is SignatureVerifier, IOracleConsumer
 		IexecODBLibCore.Task memory task = m_iexecHub.viewTask(_oracleCallId);
 		IexecODBLibCore.Deal memory deal = m_iexecClerk.viewDeal(task.dealid);
 		require(filter(task, deal));
-
-		// Parse params
-		// ... not possible until params switch from string to bytes ...
 
 		// Parse results
 		uint256       date;
@@ -84,15 +94,6 @@ contract IexecOracleReceiver is SignatureVerifier, IOracleConsumer
 			values[id].value   = value;
 		}
 	}
-
-	// function encodeParams(uint256 date, bytes memory details) public view returns(string memory)
-	// { return string(abi.encode(date, details)); }
-	//
-	// function decodeParams(string memory params) public view returns(uint256, string memory)
-	// { return abi.decode(bytes(params), (uint256, string)); }
-	//
-	// function encodeResult(uint256 date, string memory details, uint256 value) public view returns(bytes memory)
-	// { return abi.encode(date, details, value); }
 
 	function decodeResults(bytes memory results) public pure returns(uint256, string memory, uint256)
  	{ return abi.decode(results, (uint256, string, uint256)); }
