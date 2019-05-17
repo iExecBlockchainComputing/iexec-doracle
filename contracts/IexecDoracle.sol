d6c8a96f1501bbd149ec9810b91a359af35cec44
Hadrien Croubois <hc@iex.ec>
2019-05-17 14:05:44 +0200

    splitting doracle and interface (to hub & clerk)

--- contracts/IexecDoracle.sol
+++ contracts/IexecDoracle.sol
@@ -1,45 +1,21 @@
-pragma solidity ^0.5.7;
+pragma solidity ^0.5.8;
 pragma experimental ABIEncoderV2;
 
-import "iexec-poco/contracts/IexecHub.sol";
-import "iexec-poco/contracts/IexecClerk.sol";
+import "./IexecInterface.sol";
 
-contract IexecDoracle is SignatureVerifier, IOracleConsumer
+contract IexecDoracle is IexecInterface, SignatureVerifier, IOracleConsumer
 {
-	address constant IEXEC_HUB_MAINNET = 0x1383c16c927c4A853684d1a9c676986f25E22111;
-	address constant IEXEC_HUB_ROPSTEN = 0xDbe30645EA7d216c31D09f8c5736FE74de774e63;
-	address constant IEXEC_HUB_RINKEBY = 0x0000000000000000000000000000000000000000;
-	address constant IEXEC_HUB_KOVAN   = 0xb3901d04CF645747b99DBbe8f2eE9cb41A89CeBF;
-	address constant IEXEC_HUB_GOERLI  = 0x0000000000000000000000000000000000000000;
-
-	IexecHub   public m_iexecHub;
-	IexecClerk public m_iexecClerk;
-	address    public m_authorizedApp;
-	address    public m_authorizedDataset;
-	address    public m_authorizedWorkerpool;
-	bytes32    public m_requiredtag;
-	uint256    public m_requiredtrust;
+	address public m_authorizedApp;
+	address public m_authorizedDataset;
+	address public m_authorizedWorkerpool;
+	bytes32 public m_requiredtag;
+	uint256 public m_requiredtrust;
 
 	event ResultReady(bytes32 indexed doracleCallId);
 
 	constructor(address _iexecHubAddr)
-	public
-	{
-		if      (getCodeSize(_iexecHubAddr    ) > 0) { m_iexecHub = IexecHub(_iexecHubAddr    ); }
-		else if (getCodeSize(IEXEC_HUB_MAINNET) > 0) { m_iexecHub = IexecHub(IEXEC_HUB_MAINNET); }
-		else if (getCodeSize(IEXEC_HUB_ROPSTEN) > 0) { m_iexecHub = IexecHub(IEXEC_HUB_ROPSTEN); }
-		else if (getCodeSize(IEXEC_HUB_RINKEBY) > 0) { m_iexecHub = IexecHub(IEXEC_HUB_RINKEBY); }
-		else if (getCodeSize(IEXEC_HUB_KOVAN  ) > 0) { m_iexecHub = IexecHub(IEXEC_HUB_KOVAN  ); }
-		else if (getCodeSize(IEXEC_HUB_GOERLI ) > 0) { m_iexecHub = IexecHub(IEXEC_HUB_GOERLI ); }
-		else                                         { revert("invalid-hub-address");            }
-		m_iexecClerk = m_iexecHub.iexecclerk();
-	}
-
-	function getCodeSize(address _addr)
-	internal view returns (uint _size)
-	{
-		assembly { _size := extcodesize(_addr) }
-	}
+	public IexecDoracle(_iexecHubAddr)
+	{}
 
 	function receiveResult(bytes32 _doracleCallId, bytes calldata)
 	external
@@ -66,14 +42,14 @@ contract IexecDoracle is SignatureVerifier, IOracleConsumer
 	function _iexecDoracleGetVerifiedResult(bytes32 _doracleCallId)
 	internal view returns (bytes memory)
 	{
-		IexecODBLibCore.Task memory task = m_iexecHub.viewTask(_doracleCallId);
-		IexecODBLibCore.Deal memory deal = m_iexecClerk.viewDeal(task.dealid);
+		IexecODBLibCore.Task memory task = iexecHub.viewTask(_doracleCallId);
+		IexecODBLibCore.Deal memory deal = iexecClerk.viewDeal(task.dealid);
 
 		require(task.status == IexecODBLibCore.TaskStatusEnum.COMPLETED,                                                                                    "result-not-available"             );
 		require(task.resultDigest == keccak256(task.results),                                                                                               "result-not-validated-by-consensus");
-		require(m_authorizedApp        == address(0) || checkIdentity(m_authorizedApp,        deal.app.pointer,        m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-app"                 );
-		require(m_authorizedDataset    == address(0) || checkIdentity(m_authorizedDataset,    deal.dataset.pointer,    m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-dataset"             );
-		require(m_authorizedWorkerpool == address(0) || checkIdentity(m_authorizedWorkerpool, deal.workerpool.pointer, m_iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-workerpool"          );
+		require(m_authorizedApp        == address(0) || checkIdentity(m_authorizedApp,        deal.app.pointer,        iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-app"                 );
+		require(m_authorizedDataset    == address(0) || checkIdentity(m_authorizedDataset,    deal.dataset.pointer,    iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-dataset"             );
+		require(m_authorizedWorkerpool == address(0) || checkIdentity(m_authorizedWorkerpool, deal.workerpool.pointer, iexecClerk.GROUPMEMBER_PURPOSE()), "unauthorized-workerpool"          );
 		require(m_requiredtag & ~deal.tag == bytes32(0),                                                                                                    "invalid-tag"                      );
 		require(m_requiredtrust <= deal.trust,                                                                                                              "invalid-trust"                    );
 		return task.results;
