43c80f5c794eeea9794bded1aba6032deef96a80
Hadrien Croubois <hc@iex.ec>
2019-07-22 20:08:03 +0200

    adding escrow and relay methods to IexecClerkInterface

--- contracts/iexec-poco-interfaces/IexecClerkInterface.sol
+++ contracts/iexec-poco-interfaces/IexecClerkInterface.sol
@@ -3,6 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "iexec-poco/contracts/libs/IexecODBLibCore.sol";
 import "iexec-poco/contracts/libs/IexecODBLibOrders.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 
 contract IexecClerkInterface
@@ -14,12 +15,27 @@ contract IexecClerkInterface
 
 	bytes32 public /* immutable */ EIP712DOMAIN_SEPARATOR;
 
+	IERC20 public token;
+
 	event OrdersMatched        (bytes32 dealid, bytes32 appHash, bytes32 datasetHash, bytes32 workerpoolHash, bytes32 requestHash, uint256 volume);
 	event ClosedAppOrder       (bytes32 appHash);
 	event ClosedDatasetOrder   (bytes32 datasetHash);
 	event ClosedWorkerpoolOrder(bytes32 workerpoolHash);
 	event ClosedRequestOrder   (bytes32 requestHash);
 	event SchedulerNotice      (address indexed workerpool, bytes32 dealid);
+
+	event Deposit   (address owner, uint256 amount);
+	event DepositFor(address owner, uint256 amount, address target);
+	event Withdraw  (address owner, uint256 amount);
+	event Reward    (address user,  uint256 amount, bytes32 ref);
+	event Seize     (address user,  uint256 amount, bytes32 ref);
+	event Lock      (address user,  uint256 amount);
+	event Unlock    (address user,  uint256 amount);
+
+	event BroadcastAppOrder       (IexecODBLibOrders.AppOrder        apporder       );
+	event BroadcastDatasetOrder   (IexecODBLibOrders.DatasetOrder    datasetorder   );
+	event BroadcastWorkerpoolOrder(IexecODBLibOrders.WorkerpoolOrder workerpoolorder);
+	event BroadcastRequestOrder   (IexecODBLibOrders.RequestOrder    requestorder   );
 
 	function viewRequestDeals(bytes32 _id)
 	external view returns (bytes32[] memory);
@@ -64,4 +80,30 @@ contract IexecClerkInterface
 	function cancelRequestOrder(IexecODBLibOrders.RequestOrder memory _requestorder)
 	public returns (bool);
 
+	function viewAccount(address _user)
+	external view returns (IexecODBLibCore.Account memory);
+
+	function deposit(uint256 _amount)
+	external returns (bool);
+
+	function depositFor(uint256 _amount, address _target)
+	public returns (bool);
+
+	function depositForArray(uint256[] calldata _amounts, address[] calldata _targets)
+	external returns (bool);
+
+	function withdraw(uint256 _amount)
+	external returns (bool);
+
+	function broadcastAppOrder(IexecODBLibOrders.AppOrder memory _apporder)
+	public;
+
+	function broadcastDatasetOrder(IexecODBLibOrders.DatasetOrder memory _datasetorder)
+	public;
+
+	function broadcastWorkerpoolOrder(IexecODBLibOrders.WorkerpoolOrder memory _workerpoolorder)
+	public;
+
+	function broadcastRequestOrder(IexecODBLibOrders.RequestOrder memory _requestorder)
+	public;
 }
